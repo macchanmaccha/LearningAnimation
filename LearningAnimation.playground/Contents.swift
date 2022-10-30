95961968cfe6783c41eb2c916650b825b8bcdd1f
macchanmaccha <queenqueenqueen13@gmail.com>
2022-10-30 16:41:33 +0900

    withAnimation関数について

--- LearningAnimation.playground/Contents.swift
+++ LearningAnimation.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
 
+// Viewではなく、特定のプロパティの変化に対してアニメーションをつける場合は
+// withAnimation関数を使う
+
+struct ContentView: View {
+    @State private var flag = true
+    var body: some View {
+        ZStack {
+            HStack {
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 100, height: 100)
+                    .scaleEffect(flag ? 1.0 : 2.0)
+                
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .scaleEffect(flag ? 1.0 : 0.5)
+            }
+ 
+            VStack {
+                Spacer()
+                Button("アニメーション") {
+                    withAnimation() {
+                        self.flag.toggle()  // flagの変更がアニメーション化される
+                    }
+                }
+            }
+        }
+    }
+}
